925f91caa51dfa19670418602bb4ce460c5e5cbf
Saqlain Naqvi <79792270+naqviO7@users.noreply.github.com>
2023-03-28 23:13:47 +0500

    Create reqs.sh

---
+++ reqs.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+
+#commands to update linux distro and install required tools
+sudo apt-get update -y && sudo apt-get full-upgrade -y && sudo apt install lolcat -y
+chmod +x reqs.sh && chmod +x ./shrek.sh
